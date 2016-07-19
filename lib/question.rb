fc0fc738a438b859ca7fdf0c08b1cd863ac33a26
Matt Wilkin and Noah Yasskin <matt@>
2016-07-19 16:43:49 -0700

    add validation for presence and length for Question class

--- lib/question.rb
+++ lib/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to(:survey)
+  validates(:question, :presence => true, :length => { :maximum => 100})
+
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,1 +1,23 @@
 require('spec_helper')
+
+describe(Question) do
+
+  it('validates the presence of a question') do
+    question = Question.new({:question => ""})
+    expect(question.save()).to(eq(false))
+  end
+
+  it("Ensures the length of the question is at most 100 characters") do
+    question = Question.new({:question => "a".*(101)})
+    expect(question.save()).to(eq(false))
+  end
+
+describe('#survey') do
+  it("tells which survey a question belongs to") do
+    tsurvey = Survey.create({:name => "Favorite Language"})
+    tquestion = Question.create({:question => "C# or Ruby? or CSS?", :survey_id => tsurvey.id})
+    expect(tquestion.survey()).to(eq(tsurvey))
+  end
+end
+
+end
