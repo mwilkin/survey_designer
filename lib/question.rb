05d2336cbd7b2dc0e0c39e0a0899ff4b3ae9debf
Matt Wilkin and Noah Yasskin <matt@>
2016-07-19 17:06:07 -0700

    add downcase method to question with passing spec

--- lib/question.rb
+++ lib/question.rb
@@ -1,5 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to(:survey)
+  before_save(:downcase_question)
   validates(:question, :presence => true, :length => { :maximum => 100})
 
+
+
+  private
+    define_method(:downcase_question) do
+      self.question = (question().downcase())
+  end
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -2,6 +2,11 @@ require('spec_helper')
 
 describe(Question) do
 
+  it('changes the case of the question to all lowercase') do
+    question1 = Question.create({:question => "HOW DO YOU LIKE YOUR STABBY LAMBDA?"})
+    expect(question1.question()).to(eq("how do you like your stabby lambda?"))
+  end
+
   it('validates the presence of a question') do
     question = Question.new({:question => ""})
     expect(question.save()).to(eq(false))
